e228040c3058058432e738947d3240fc30b654a0
kangbc816@gmail.com <bcbc1257!@>
2021-06-06 09:30:28 +0900

    검색되지 않는 상점 검색 가능하도록 수정 가게 설명 네이버 지도에서 가져오도록 수정

--- src/main/java/com/aa/crawlingweb/util/SearchUtil.java
+++ src/main/java/com/aa/crawlingweb/util/SearchUtil.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.springframework.stereotype.Component;
@@ -47,6 +48,16 @@ public class SearchUtil {
             store.setPlaceName(driver.findElement(By.className("_3XamX")).getText());
             store.setCategory(driver.findElement(By.className("_3ocDE")).getText());
             try {
+                driver.findElement(By.cssSelector("li._1M_Iz._3__3i .WoYOw")).click();
+                store.setDescription(driver.findElement(By.cssSelector("li._1M_Iz._3__3i .WoYOw")).getText());
+            } catch (NoSuchElementException e) {
+            }
+            try {
+                store.setAddress(driver.findElement(By.className("_2yqUQ")).getText());
+            } catch (NoSuchElementException e) {
+            }
+
+            try {
                 List<WebElement> elemUndefineds = driver.findElements(By.cssSelector("li._1M_Iz.undefined"));
                 for(WebElement elemUndefined : elemUndefineds) {
                     if(elemUndefined.getText().contains("편의")) {
@@ -77,6 +88,29 @@ public class SearchUtil {
 
     private boolean getDetailInfo(WebDriver driver, Store storeData, CrawlingResponse database) {
         String storeInfoUrl = "https://map.naver.com/v5/api/search?query=" + storeData.getPlaceName().replace(" ", "%20");
+
+        List<Store> storeList = getStoreData(driver, storeData, storeInfoUrl);
+
+        if(storeList.size() == 0) {
+            storeList.addAll(getDetailInfoByStoreLocation(driver, storeData));
+        }
+        CrawlingChoices choices = CrawlingChoices.builder()
+                .placeName(storeData.getPlaceName())
+                .mapUrl(storeData.getMapUrl())
+                .choices(storeList)
+                .uuid(UUID.randomUUID().toString().replaceAll("-", ""))
+                .build();
+        database.getStores().add(choices);
+        return true;
+    }
+
+    public List<Store> getDetailInfoByStoreLocation(WebDriver driver, Store storeData) {
+        String searchName = (storeData.getPlaceName() + " " + storeData.getAddress()).replace(" ", "%20");
+        String storeInfoUrl = "https://map.naver.com/v5/api/search?query=" + searchName;
+        return getStoreData(driver, storeData, storeInfoUrl);
+    }
+
+    public List<Store> getStoreData(WebDriver driver, Store storeData, String storeInfoUrl) {
         try {
             driver.get(storeInfoUrl);
 
@@ -89,7 +123,7 @@ public class SearchUtil {
             for(Detail detail : details) {
                 // 아이디가 동일한 업체 선택
                 if(detail.getId() != storeData.getId()) {
-                   continue;
+                    continue;
                 }
                 String tel = null;
                 if(!StringUtils.isEmpty(detail.getVirtualTel())) {
@@ -105,7 +139,11 @@ public class SearchUtil {
                 store.setTel(tel);
                 store.setBusinessHour(!StringUtils.isEmpty(detail.getBizhourInfo()) ? detail.getBizhourInfo() : null);
                 store.setHomepage(!StringUtils.isEmpty(detail.getHomePage()) ? detail.getHomePage() : null);
-                store.setDescription((detail.getMicroReview() != null && detail.getMicroReview().size() > 0) ? detail.getMicroReview().get(0) : null);
+                if(StringUtils.isEmpty(storeData.getDescription())) {
+                    store.setDescription((detail.getMicroReview() != null && detail.getMicroReview().size() > 0) ? detail.getMicroReview().get(0) : null);
+                } else {
+                    store.setDescription(storeData.getDescription());
+                }
                 store.setConvenience(storeData.getConvenience());
                 store.setShortAddress((detail.getShortAddress() != null && detail.getShortAddress().size() > 0) ? detail.getShortAddress().get(0) : null);
                 store.setAddress(!StringUtils.isEmpty(detail.getAddress()) ? detail.getAddress() : null);
@@ -115,17 +153,10 @@ public class SearchUtil {
                 storeList.add(store);
                 loggingUtil.writeFile("[Store] " + store);
             }
-            CrawlingChoices choices = CrawlingChoices.builder()
-                    .placeName(storeData.getPlaceName())
-                    .mapUrl(storeData.getMapUrl())
-                    .choices(storeList)
-                    .uuid(UUID.randomUUID().toString().replaceAll("-", ""))
-                    .build();
-            database.getStores().add(choices);
-            return true;
+            return storeList;
         } catch (Exception e) {
             loggingUtil.writeFile("[Failed Get Store Info] API URL - " + storeInfoUrl);
-            return false;
+            return new ArrayList<>();
         }
     }
 
